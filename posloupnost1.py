5561f22be6844474cff581619af320ba61008569
Galadir <danpumr@gmail.com>
2019-02-17 15:42:00 +0100

    doplnění komentářů

--- posloupnost1.py
+++ posloupnost1.py
@@ -1,10 +1,12 @@
+# dotázání uživatele na možnost práce s desetinnými čísly
 des = input('Napište "ANO", pokud chcete pracovat s desetinými čísly: ')
 
-
+# dotázání uživatele na vstupní posloupnosti
 print("Zapište posloupnosti. Čísla oddělte mezerou.")
 a=input("Posloupnost 1: ")
 b=input("Posloupnost 2: ")
 
+# převedení hodnot na množiny čísel a ošetření pro případ zadání nebhodných hodnot
 try:
     if des=="ANO" or des=="ano":
         a = set(map(float, a.split()))
@@ -18,9 +20,11 @@ except ValueError:
           "\nProgram byl ukončen")
     exit(2)
 
+# provedení sjednocení
 c={}
 c=a.union(b)
 
+# vytisknutí výsledné posloupnosti
 print("Sjednocení posloupností 1 a 2 je {}.".format(sorted(c)))
 
 
