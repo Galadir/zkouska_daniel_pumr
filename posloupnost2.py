8cec9b0ff9c8e90cf8f2fd2d1a4820dc5526b4d0
Galadir <danpumr@gmail.com>
2019-02-17 15:44:38 +0100

    doplnění komentářů

--- posloupnost2.py
+++ posloupnost2.py
@@ -1,10 +1,12 @@
+# dotázání uživatele na možnost práce s desetinnými čísly
 des = input('Napište "ANO", pokud chcete pracovat s desetinými čísly: ')
 
-
+# dotázání uživatele na vstupní posloupnosti
 print("Zapište posloupnosti. Čísla oddělte mezerou.")
 a=input("Posloupnost 1: ")
 b=input("Posloupnost 2: ")
 
+# převedení hodnot na seznamy čísel a ošetření pro případ zadání nebhodných hodnot
 try:
     if des=="ANO" or des=="ano":
         a = list(map(float, a.split()))
@@ -18,11 +20,13 @@ except ValueError:
           "\nProgram byl ukončen")
     exit(2)
 
+# tvorba seznamů pro průnik a doplňky
 prunik=[]
 
 doplnek_a=[]
 doplnek_b=[]
 
+# cykly rozřazující hodnoty do průniku nebo do doplnků
 for i in a:
     if i in b:
         prunik.append(i)
@@ -33,8 +37,10 @@ for i in b:
     if i not in a:
         doplnek_b.append(i)
 
+# vytvoření sjednocení
 sjednoceni=sorted(doplnek_a+prunik+doplnek_b)
 
+# vytisknutí výsledných posloupností
 print("Pro zadané posloupnosti je sjednocení tvořeno hodnotami {}\n"
       "průnik = {}\n"
       "doplněk poslupnosti 1 = {}\n"
